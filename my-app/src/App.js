b9e79f90e43e080618a13622638fa9980b7bbd45
程序员云锦 <lsl12120927@163.com>
2022-12-20 21:22:59 +0800

    部分提交

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import MyComponent1 from './component/MyComponent1'
+function App() {
+  return (
+    <div className="App">
+      <MyComponent1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/component/MyComponent1/index.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+class MyComponent1 extends React.Component {
+  render() {
+    return (
+      <div>MyComponent1</div>
+    )                                                                                                                                                                      )
+  }
+}
+export default MyComponent1
